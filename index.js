aebadb1a8856e5edf6ba82c142bace3cb325fbbd
Kirstie <kirstiewithrow@gmail.com>
2021-04-27 20:26:55 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return  a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(a) {
+    return a + 1;
+}
+function decrement(a) {
+    return a - 1;
+}
+
+const makeInt = function(n) {
+    return parseInt(n, 10);
+     
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}   
